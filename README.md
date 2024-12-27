f746658491d0209939b4840479e3f1fd2aad9881
TrueDarkLord <truedarklord.email@gmail.com>
2024-12-27 13:24:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# What is this plugin?
+It's a simple plugin that lets you limit what the crafter can make.
+
+# Features
+- Blacklist for the crafter.
+
+The current version of the plugin was intentionally kept simple to reduce the likelihood of it breaking between different Minecraft versions.
+
+# Setup:
+- Download the jar file.
+- Place the file into your server's plugin folder.
+- Start the server and Enjoy!
+
+### Tested + Working Versions:
+  - ✅ Tested on 1.21.1
+
+
+## Feel free to open an [issue](https://github.com/TrueDarkLord/CRafterBlacklist/issues) if there are problems.
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/O4O0OI3A2)
---
+++ src/main/java/me/truedarklord/crafterBlacklist/CrafterBlacklist.java
@@ -0,0 +1,47 @@
+package me.truedarklord.crafterBlacklist;
+
+import me.truedarklord.crafterBlacklist.metrics.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrafterBlacklist extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Metrics metrics = new Metrics(this, 24251);
+        saveDefaultConfig();
+
+        advertise();
+
+    }
+
+    private void advertise() {
+        this.getServer().getConsoleSender().sendMessage(
+                """
+  
+ §#00AA00================================§#ee2222
+
+ _____            __ _             \s
+ / ____|          / _| |            \s
+| |     _ __ __ _| |_| |_ ___ _ __  \s
+| |____| | | (_| | | | ||  __/ |    \s
+| |    | '__/ _` |  _| __/ _ \\ '__| \s
+ \\_____|_|  \\__,_|_|_ \\__\\___|_| _  \s
+|  _ \\| |          | |  | (_)   | | \s
+| |_) | | __ _  ___| | _| |_ ___| |_\s
+|  _ <| |/ _` |/ __| |/ / | / __| __|
+| |_) | | (_| | (__|   <| | \\__ \\ |_\s
+|____/|_|\\__,_|\\___|_|\\_\\_|_|___/\\__|
+                                    \s
+                                    \s
+                        
+
+§#f5da2aBy TrueDarkLord.
+§#00AA00================================
+§#f5da2aFeel free to buy me a coffee:  §#00AA00|
+§bhttps://ko-fi.com/truedarklord §#00AA00|
+§#00AA00================================
+                        """
+        );
+    }
+
+}
